30f11c3b3fb2c771c4ceeb33dbc6ed0ff456075f
William Tomé <william.tomenh@gmail.com>
2023-04-03 23:28:01 -0300

    refactor: change code.using constant

--- src/Search.php
+++ src/Search.php
@@ -3,15 +3,13 @@
 namespace WilliamTome\DigitalCep;
 
 class Search {
-    
-    private $url = "https://viacep.com.br/ws/";
+    public const URL = "https://viacep.com.br/ws/";
 
     public function getAddressFromZipcode(string $zipCode): array
     {
-        
         $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
-        $get = file_get_contents($this->url . $zipCode . "/json");
+        $get = file_get_contents(self::URL . $zipCode . "/json");
 
         return (array) json_decode($get);
     }
