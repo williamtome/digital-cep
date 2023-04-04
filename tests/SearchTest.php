91273b4258292aeb79dde3305dc808138756cc75
William Tomé <william.tomenh@gmail.com>
2023-04-03 23:28:33 -0300

    test: improve test case using data provider.

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -7,32 +7,52 @@ use WilliamTome\DigitalCep\Search;
 
 class SearchTest extends TestCase
 {
-    public function testGetAddressFromZipcode()
+    /**
+     * @dataProvider addresses
+     */
+    public function testGetAddressFromZipcode(string $input, array $expected)
     {
-        // Arrange
-        $zipcode = "01001000";
-
         // Act
         $search = new Search;
-        $result = $search->getAddressFromZipcode($zipcode);
+        $result = $search->getAddressFromZipcode($input);
 
         // Assert
-        $this->assertEquals($this->address(), $result);
+        $this->assertEquals($expected, $result);
     }
 
-    private function address()
+    public function addresses()
     {
         return [
-            'cep' => "01001-000",
-            "logradouro" => "Praça da Sé",
-            "complemento" => "lado ímpar",
-            "bairro" => "Sé",
-            "localidade" => "São Paulo",
-            "uf" => "SP",
-            "ibge" => "3550308",
-            "gia" => "1004",
-            "ddd" => "11",
-            "siafi" => "7107",
+            'Endereço Praça da Sé' => [
+                '01001000', 
+                [
+                    'cep' => "01001-000",
+                    "logradouro" => "Praça da Sé",
+                    "complemento" => "lado ímpar",
+                    "bairro" => "Sé",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP",
+                    "ibge" => "3550308",
+                    "gia" => "1004",
+                    "ddd" => "11",
+                    "siafi" => "7107",
+                ],
+            ],
+            'Endereço Qualquer' => [
+                '43325534', 
+                [
+                    'cep' => "43325-534",
+                    "logradouro" => "Praça da Sé",
+                    "complemento" => "lado ímpar",
+                    "bairro" => "Sé",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP",
+                    "ibge" => "3550308",
+                    "gia" => "1004",
+                    "ddd" => "11",
+                    "siafi" => "7107",
+                ],
+            ],
         ];
     }
 }
